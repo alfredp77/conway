ab3f851996fd411e06017788e822012dbba3a79b
Alfred Padawangi <alfredp77@outlook.com>
2023-05-13 10:29:26 +0800

    seems like basic logic is there

--- Conway.Main/GameRunner.cs
+++ Conway.Main/GameRunner.cs
@@ -11,6 +11,44 @@ public class GameRunner : IGameRunner
 
     public GameState GenerateNextState(GameState current)
     {
-        return current with { LiveCells = new List<Point>() };
+        var nextLiveCells = new List<Point>();
+        for (var x=1; x<=current.Parameters.Width; x++)
+        {
+            for (var y=1; y<=current.Parameters.Height; y++)
+            {
+                var cell = new Point(x, y);
+                var neighbours = GetNeighbours(cell, current.LiveCells);
+                switch (neighbours.Count)
+                {
+                    case 3:
+                    case 2 when current.LiveCells.Contains(cell):
+                        nextLiveCells.Add(cell);
+                        break;
+                }
+            }
+        }
+        
+        return current with { LiveCells = nextLiveCells };
+    }
+
+    private IReadOnlyList<Point> GetNeighbours(Point cell, ICollection<Point> currentLiveCells)
+    {
+        var neighbours = new List<Point>();
+        for (var x = cell.X - 1; x <= cell.X + 1; x++)
+        {
+            for (var y = cell.Y - 1; y <= cell.Y + 1; y++)
+            {
+                if (x == cell.X && y == cell.Y)
+                {
+                    continue;
+                }
+                var neighbour = new Point(x, y);
+                if (currentLiveCells.Contains(neighbour))
+                {
+                    neighbours.Add(neighbour);
+                }
+            }
+        }
+        return neighbours;
     }
 }
--- Conway.Tests/GameRunnerTests.cs
+++ Conway.Tests/GameRunnerTests.cs
@@ -38,4 +38,64 @@ public class GameRunnerTests
 
         Assert.Empty(nextState.LiveCells);
     }
+    
+    [Fact]
+    public void Cell_With_Two_Neighbours_Should_Survive_In_Next_State()
+    {
+        var state = new GameState
+        {
+            Parameters = _testParameters,
+            LiveCells = new List<Point> { new(2, 2), new(2, 3), new(3, 2)}
+        };
+
+        var nextState = _runner.GenerateNextState(state);
+
+        Assert.Contains(new Point(2,2), nextState.LiveCells);
+        Assert.Contains(new Point(2,3), nextState.LiveCells);
+        Assert.Contains(new Point(3,2), nextState.LiveCells);
+    }
+    
+    [Fact]
+    public void Cell_With_Three_Neighbours_Should_Survive_In_Next_State()
+    {
+        var state = new GameState
+        {
+            Parameters = _testParameters,
+            LiveCells = new List<Point> { new(2, 2), new(2, 3), new(3, 2), new(3, 3)}
+        };
+
+        var nextState = _runner.GenerateNextState(state);
+
+        Assert.Equal(new List<Point> { new(2, 2), new(2, 3), new(3, 2), new(3, 3)}, nextState.LiveCells);
+    }
+    
+    [Fact]
+    public void Cell_With_More_Than_Three_Neighbours_Should_Die_In_Next_State()
+    {
+        var state = new GameState
+        {
+            Parameters = _testParameters,
+            LiveCells = new List<Point> { new(2, 2), new(2, 3), new(3, 2), new(3, 3), new(4, 2)}
+        };
+
+        var nextState = _runner.GenerateNextState(state);
+
+        Assert.DoesNotContain(new Point(3,2), nextState.LiveCells);
+        Assert.DoesNotContain(new Point(3,3), nextState.LiveCells);
+    }
+    
+    [Fact]
+    public void Dead_Cell_With_Three_Neighbours_Should_Become_Alive_In_Next_State()
+    {
+        var state = new GameState
+        {
+            Parameters = _testParameters,
+            LiveCells = new List<Point> { new(2, 2), new(2, 3), new(3, 2)}
+        };
+
+        var nextState = _runner.GenerateNextState(state);
+
+        Assert.Equal(new List<Point> { new(2, 2), new(2, 3), new(3, 2), new(3, 3)}, nextState.LiveCells);
+    }
+    
 }
