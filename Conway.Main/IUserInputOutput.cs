ea1609189b9b19847d52fb3e131fe7e9bbb0a4c3
Alfred Padawangi <alfredp77@outlook.com>
2023-05-13 18:12:12 +0800

    add md file

--- Conway.Main/IUserInputOutput.cs
+++ Conway.Main/IUserInputOutput.cs
@@ -5,3 +5,16 @@ public interface IUserInputOutput
     void WriteLine(string textToDisplay="");
     string ReadLine();
 }
+
+public class ConsoleUserInputOutput : IUserInputOutput
+{
+    public void WriteLine(string textToDisplay="")
+    {
+        Console.WriteLine(textToDisplay);
+    }
+
+    public string ReadLine()
+    {
+        return Console.ReadLine() ?? "";
+    }
+}
