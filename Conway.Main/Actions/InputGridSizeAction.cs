1020e8192043ea387586eb8d15b58767b21f47f4
Alfred Padawangi <alfredp77@outlook.com>
2023-05-18 22:28:21 +0800

    refactor InputGridSizeAction

--- Conway.Main/Actions/InputGridSizeAction.cs
+++ Conway.Main/Actions/InputGridSizeAction.cs
@@ -18,35 +18,41 @@ public class InputGridSizeAction : IAction
     public string Description => "Specify grid size";
     public GameParameters Execute(GameParameters gameParameters)
     {
-        GameParameters? newParameters = null;
-        while (newParameters == null)
+        var newParameters = gameParameters;
+        var shouldGetInput = true;
+        while (shouldGetInput)
         {
-            newParameters = GetInput(gameParameters);
+            _userInputOutput.WriteLine(Prompt);
+            var input = _userInputOutput.ReadLine();
+            if (input == Command.Exit)
+            {
+                break;
+            }
+
+            (newParameters, shouldGetInput, var isValid) = ProcessInput(input, newParameters);
+            if (!isValid)
+            {
+                _userInputOutput.WriteLine(CommonMessages.InvalidInputMessage);
+            }
         }
+
         return newParameters;
     }
 
-    private GameParameters? GetInput(GameParameters gameParameters)
+    private (GameParameters newParameters, bool shouldGetInput, bool isValid) ProcessInput(string input, GameParameters gameParameters)
     {
-        _userInputOutput.WriteLine(Prompt);
-        var input = _userInputOutput.ReadLine();
-        if (input == Command.Exit)
-        {
-            return gameParameters;
-        }
-        
         var tokens = input.Split(' ');
         if (int.TryParse(tokens[0], out var parsedWidth) && int.TryParse(tokens[1], out var parsedHeight))
         {
             if ((gameParameters.MaxWidth == 0 || (parsedWidth <= gameParameters.MaxWidth && parsedWidth > 0)) &&
                 (gameParameters.MaxHeight == 0 || (parsedHeight <= gameParameters.MaxHeight && parsedHeight > 0)))
             {
-                return gameParameters with {Width = parsedWidth, Height = parsedHeight};
+                return (gameParameters with {Width = parsedWidth, Height = parsedHeight}, false, true);
             }
         }
             
-        _userInputOutput.WriteLine(CommonMessages.InvalidInputMessage);
-        return null;
+        
+        return (gameParameters, true, false);
     }
 
 }
